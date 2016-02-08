40048073d34a3199dad2e07fa159bf45a06c8682
mbch331 <mbch331@users.noreply.github.com>
2016-02-08 16:32:43 +0100

    Update index.php
    Hidden form field

--- public_html/index.php
+++ public_html/index.php
@@ -14,6 +14,7 @@ if (!isset($_POST)||empty($_POST)) //No information was submitted, so need to sh
 	<p id="information">Currently only English and Dutch property names/item names (if they need to be translated into QID's) are accepted. Dates need to be in format: d-m-Y (no trailing zeros needed, but no problem if they are added. First row needs to be the properties, other rows need to be the statements.</p>
 	<form action="index.php" action="POST" enctype="multipart/form-data">
 	<textarea name="data"></textarea>
+	<input type="hidden" name="verzonden" value="1">
 	<input type="submit" value="Verwerk">
 	</form>
 	</body>
